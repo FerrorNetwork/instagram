03508d35a217816d8a959e20892bc492783090ee
Danil <suncovdanil727@gmail.com>
2023-01-18 15:36:13 +0400

    init

---
+++ instagram/Feed/FeedViewController.swift
@@ -0,0 +1,18 @@
+//
+//  FeedViewController.swift
+//  instagram
+//
+//  Created by Данил on 18.01.2023.
+//
+import SnapKit
+import UIKit
+
+class FeedViewController: UIViewController {
+    //MARK: - view lifecycle
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .red
+    }
+}
+
+//MARK: - private extension
